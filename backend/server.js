2bc2493e009b55a81a8eff031280ae615f920aa7
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-07 19:22:58 +0100

    secure connection with mongoDb backend established

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import userRoutes from './routes/userRoutes'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT || 3002
-const app = express()
+const port = process.env.PORT || 3002;
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/users', userRoutes)
+app.use(cors());
+app.use(express.json());
+app.use("/api/users", userRoutes);
 
-app.get('/',(req,res)=> res.send('Server is ready'))
+app.get("/", (req, res) => res.send("Server is ready"));
 
-app.listen(port,()=> console.log(`Server started on http://localhost:${port}`))
+mongoose.connect(
+  "mongodb+srv://darnzykay:myiT8U83fT0qqGAf@doclane.s4nfpml.mongodb.net/?retryWrites=true&w=majority"
+)
+  ? app.listen(port, () =>
+      console.log(`Server started on http://localhost:${port}`)
+    )
+  : app.listen(port, () =>
+  console.log(`Server Connection Error`)
+)
