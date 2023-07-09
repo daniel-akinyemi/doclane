a6f70a6334fe0ebf6298a59af2820ca94f4065a6
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-09 17:03:25 +0100

    minor update

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -7,10 +7,6 @@ const page = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const startSession = (e) => {
-    e.preventDefault();
-  };
-
   return (
     <section className="flex flex-col md:flex-row max-w-5xl mx-auto my-12 items-center align-middle md:space-x-8">
       <div className=" text-white w-4/5 md:w-1/2 hidden md:flex flex-col items-center justify-center space-y-8 align-middle">
@@ -21,25 +17,22 @@ const page = () => {
           className="rounded-lg"
         />
       </div>
-      <form
-        onSubmit={startSession}
-        className="bg-gray-50 h-96 border-gray-100 border-[1px] rounded-2xl w-4/5 md:w-1/2 flex flex-col items-center justify-center space-y-8"
-      >
+      <form className="bg-white h-96 border-gray-100 border-[1px] rounded-2xl w-4/5 md:w-1/2 flex flex-col items-center justify-center space-y-8">
         <input
           type="text"
           placeholder="Email"
-          className="p-4 rounded-md"
+          className="p-4 rounded-md border-[1px] border-gray-200"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="Password"
-          className="p-4 rounded-md"
+          className="p-4 rounded-md border-[1px] border-gray-200"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="px-24 py-4 rounded-md bg-purple-700 text-white">
+        <button type="submit" className="px-24 py-4 rounded-md w-3/6 bg-purple-700 text-white">
           Login
         </button>
       </form>
