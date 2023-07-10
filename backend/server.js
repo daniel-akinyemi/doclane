91bcb9e7977a660bcf704eb163c28dc42d01a35d
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-10 20:34:11 +0100

    register api route connection with backend established

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,17 @@ import router from "./routes/userRoutes.js";
 
 dotenv.config();
 
-const port = process.env.PORT || 3002;
+const port = 3002;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", router);
 
-app.get("/", (req, res) => res.send("Server is ready"));
+// app.get("/", (req, res) => res.send("Server is ready"));
 
 mongoose.connect(
-  "mongodb+srv://darnzykay:myiT8U83fT0qqGAf@doclane.s4nfpml.mongodb.net/?retryWrites=true&w=majority"
+  "mongodb+srv://darnzykay:myiT8U83fT0qqGAf@doclane.s4nfpml.mongodb.net/doclane?retryWrites=true&w=majority"
 )
   ? app.listen(port, () =>
       console.log(`Server started on http://localhost:${port}`)
