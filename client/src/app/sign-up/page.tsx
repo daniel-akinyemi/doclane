dcf1df24f95e44a338fcdb68459e104c642c0b54
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-11 17:34:29 +0100

    Sign-up page connected to backend

--- client/src/app/sign-up/page.tsx
+++ client/src/app/sign-up/page.tsx
@@ -2,15 +2,27 @@
 import { useState } from "react";
 import loginImage from "../../../public/loginImage.png";
 import Image from "next/image";
+import axios from 'axios'
 
 const page = () => {
 
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+    try{
+      axios.post('http://localhost:3002/api/users/register',{username,password})
+      alert("user created.")
+    }catch(err){
+      console.log(`message: ${err}`)
+      alert(err)
+    }
+  }
 
   return (
-    <section className="flex flex-col md:flex-row max-w-5xl mx-auto my-12 items-center align-middle md:space-x-8">
+    <section onSubmit={handleSubmit}
+    className="flex flex-col md:flex-row max-w-5xl mx-auto my-12 items-center align-middle md:space-x-8">
       <div className=" text-white w-4/5 md:w-1/2 hidden md:flex flex-col items-center justify-center space-y-8 align-middle">
         <Image
           src={loginImage}
@@ -24,15 +36,14 @@ const page = () => {
           type="text"
           placeholder="Email"
           className="p-4 rounded-md border-[1px] border-gray-200"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          
         />
         <input
           type="text"
           placeholder="Username"
           className="p-4 rounded-md border-[1px] border-gray-200"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
         <input
           type="password"
@@ -45,8 +56,7 @@ const page = () => {
           type="password"
           placeholder="Confirm password"
           className="p-4 rounded-md border-[1px] border-gray-200"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          
         />
         <button type="submit" className="px-16 py-4 rounded-md w-3/6 bg-purple-700 text-white">
           Sign Up
