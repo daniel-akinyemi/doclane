d6f19f34e43b7e9dbeffc7f7ed4c62fac1d5086a
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-11 17:34:05 +0100

    hompage design updated

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='flex justify-center py-16 text-2xl font-bold'>
-      Modern Solution for Documents Management
+    <main className='flex flex-col justify-center py-16 text-5xl md:text-7xl font-extrabold w-4/6 mx-auto'>
+      <span className='text-purple-700'>Modern Solution</span> for Documents Management
     </main>
   )
 }
