7397414868c2ce79fa34de77def1fb5f8c1b59a7
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-15 14:25:13 +0100

    minor updates to login page layout

--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -15,7 +15,7 @@ const page = () => {
   return (
     <main className='bg-gray-100'>
         <section className='w-[90%] mx-auto my-2 py-8 flex space-x-4 '>
-            <aside className='w-1/6 p-4 flex flex-col justify-between bg-white h-96 rounded-lg'>
+            <aside className='w-2/6 md:1/6 p-4 flex flex-col justify-between bg-white h-96 rounded-lg'>
                 <div className='flex flex-col space-y-4 items-start'>
                 <button>Homepage</button>
                 <button>Settings</button>
