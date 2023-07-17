6c653b0177519437285fff584a818923beae232f
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-17 19:40:33 +0100

    create document page updated

--- client/src/app/create-document/page.tsx
+++ client/src/app/create-document/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <form className="space-y-4 w-10/12 mx-auto">
+        <div>
+          <label htmlFor="title"></label>
+          <input type="text" id="title" placeholder="Title" className="px-4 py-2"/>
+        </div>
+        <div>
+          <label htmlFor="description"></label>
+          <input type="text" id="description" placeholder="Description" className="px-4 py-2"/>
+        </div>
+        <div>
+          <label htmlFor="body"></label>
+          <input type="text" id="body" placeholder="Body" className="px-4 py-2"/>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default page
+export default page;
