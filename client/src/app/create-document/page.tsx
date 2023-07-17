8527a8ddbbf80393e567c8c2eeef969fc6d47d5c
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-17 19:42:47 +0100

    ux update to create-document page

--- client/src/app/create-document/page.tsx
+++ client/src/app/create-document/page.tsx
@@ -3,18 +3,18 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <form className="space-y-4 w-10/12 mx-auto">
+      <form className="space-y-4 w-10/12 mx-auto my-8">
         <div>
           <label htmlFor="title"></label>
-          <input type="text" id="title" placeholder="Title" className="px-4 py-2"/>
+          <input type="text" id="title" placeholder="Title" className="px-4 py-2 border-2 border-gray-500"/>
         </div>
         <div>
           <label htmlFor="description"></label>
-          <input type="text" id="description" placeholder="Description" className="px-4 py-2"/>
+          <input type="text" id="description" placeholder="Description" className="px-4 py-2 border-2 border-gray-500"/>
         </div>
         <div>
           <label htmlFor="body"></label>
-          <input type="text" id="body" placeholder="Body" className="px-4 py-2"/>
+          <input type="text" id="body" placeholder="Body" className="px-4 py-2 border-2 border-gray-500"/>
         </div>
       </form>
     </div>
