96506c32184df29c6be1709c8f7055e0c3eba04d
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-21 20:53:17 +0100

    singleDocument page updated

--- client/src/components/SingleDocuments.tsx
+++ client/src/components/SingleDocuments.tsx
@@ -1,7 +1,14 @@
 
+
+
 const SingleDocuments = () => {
   return (
-    <div>SingleDocuments</div>
+    <div>
+      <div className="flex space-x-12 bg-gray-200 rounded-lg py-4 px-8 my-4" >
+        <div className="font-bold">1.</div>
+        <div className="font-bold">This is a test document</div>
+      </div>
+    </div>
   )
 }
 
