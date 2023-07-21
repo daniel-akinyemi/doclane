eea3deed6e3285b0135ea819608ce42120b4156b
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-21 20:53:02 +0100

    profile page updated

--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -17,11 +17,11 @@ const page = () => {
         <section className='w-[90%] mx-auto my-2 py-8 flex space-x-4 '>
             <aside className='w-2/6 md:1/6 p-4 flex flex-col justify-between bg-white h-96 rounded-lg'>
                 <div className='flex flex-col space-y-4 items-start text-white'>
-                <button>Homepage</button>
-                <button>Settings</button>
+                <button className="bg-gray-700 rounded-md w-3/5 text-left px-16 py-4 my-4">Homepage</button>
+                <button className="bg-gray-700 rounded-md w-3/5 text-left px-16 py-4 my-4">Settings</button>
                 </div>
                 <div>
-                    <button onClick={handleLogOut} className="bg-gray-700 text-white px-8 py-2 rounded-lg">Log Out</button>
+                    <button onClick={handleLogOut} className="bg-gray-700 text-white w-3/5 px-16 py-4 rounded-md">Log Out</button>
                 </div>
             </aside>
             <div className='w-4/6 p-4 bg-white text-white'> This is the div</div>
