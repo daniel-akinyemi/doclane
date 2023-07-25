efeb6a4cb0f5bab35b014fa6e72366967a051dd7
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-25 19:34:11 +0100

    store.js file created

--- client/src/components/SingleDocuments.tsx
+++ client/src/components/SingleDocuments.tsx
@@ -1,15 +1,12 @@
-
-
-
-const SingleDocuments = ({document}) => {
+const SingleDocuments = ({ document }) => {
   return (
     <div>
-      <div className="flex space-x-12 bg-gray-200 rounded-lg py-4 px-8 my-4" >
+      <div className="flex space-x-12 bg-gray-200 rounded-lg py-4 px-8 my-4">
         <div className="font-bold">{document._id}</div>
         <div className="font-bold">{document.title}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SingleDocuments
+export default SingleDocuments;
