42ea4d7dacacf4a614a8c044e160b26410a23776
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-27 23:47:16 +0100

    document id added in background

--- client/src/components/SingleDocuments.tsx
+++ client/src/components/SingleDocuments.tsx
@@ -4,6 +4,7 @@ const SingleDocuments = ({ document,index }) => {
       <div className="flex space-x-12 bg-gray-200 rounded-lg py-4 px-8 my-4">
         <div className="font-bold">{index + 1}</div>
         <div className="font-bold">{document.title}</div>
+        <span className="hidden">{document._id}</span>
       </div>
     </div>
   );
